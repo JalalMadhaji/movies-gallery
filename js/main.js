a70fcf6020e732d9672b5295eb1587b8aa69036c
JalalMadhaji <jalal.almadhaji90@gmail.com>
2019-12-18 16:59:25 +0300

    comment the slider

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   let movies = document.getElementById("movies");
-  let sliderMovies = document.getElementById("cards");
+  //   let sliderMovies = document.getElementById("cards");
 
   function fetchMovies() {
     fetch("http://api.tvmaze.com/shows")
@@ -22,27 +22,27 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   fetchMovies();
 
-  function fetchSliderMovies() {
-    fetch("http://api.tvmaze.com/shows")
-      .then(res => res.json())
-      .then(data => {
-        let slideArray = [];
-        for (let i = 0; i < 11; i++) {
-          slideArray.push(data[i]);
-        }
-        slideArray.forEach(movie => {
-          let card = document.createElement("div");
-          card.className = "card-item";
-          let img = document.createElement("img");
-          img.src = movie.image.medium;
-          let h3 = document.createElement("h3");
-          h3.innerHTML = movie.name;
-          card.appendChild(img);
-          card.appendChild(h3);
-          sliderMovies.appendChild(card);
-        });
-      })
-      .catch(err => console.log(err));
-  }
-  fetchSliderMovies();
+  //   function fetchSliderMovies() {
+  //     fetch("http://api.tvmaze.com/shows")
+  //       .then(res => res.json())
+  //       .then(data => {
+  //         let slideArray = [];
+  //         for (let i = 0; i < 11; i++) {
+  //           slideArray.push(data[i]);
+  //         }
+  //         slideArray.forEach(movie => {
+  //           let card = document.createElement("div");
+  //           card.className = "card-item";
+  //           let img = document.createElement("img");
+  //           img.src = movie.image.medium;
+  //           let h3 = document.createElement("h3");
+  //           h3.innerHTML = movie.name;
+  //           card.appendChild(img);
+  //           card.appendChild(h3);
+  //           sliderMovies.appendChild(card);
+  //         });
+  //       })
+  //       .catch(err => console.log(err));
+  //   }
+  //   fetchSliderMovies();
 });
